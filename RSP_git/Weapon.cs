2fa5a278fb6d1fca2dbe9488d7de6382b255b093
PEPEKT0N <slavon.96.96@mail.ru>
2024-10-02 19:56:22 +0300

    add simple menu

---
+++ RSP_git/Weapon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSP_git
+{
+    enum Weapon
+    {
+        Rock = 0,
+        Scissor = 1,
+        Paper = 2
+    }
+}
